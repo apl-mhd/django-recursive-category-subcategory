97477a5fc6a09b2048eee0517d396cad600f6460
Apel Mahmud <44221242+apl-mhd@users.noreply.github.com>
2022-02-03 03:35:09 +0600

    variable name change

--- base/views.py
+++ base/views.py
@@ -11,23 +11,23 @@ def index(request):
     catagories = Category.objects.filter(parent = NULL)
     htmltag = "<ul>"
 
-    def rec(i, x): 
-        child = Category.objects.filter(parent = i )
+    def recur(id, x): 
+        child = Category.objects.filter(parent = id )
         if(len(child) != 0):
-            x +="<ul>"
-            for i in child:
-                x +="<li>"+ str(i)
-                x =rec(i.id, x)
+            x += "<ul>"
+            for j in child:
+                x += "<li>"+ j.name
+                x = recur(j.id, x)
             
             x +="</ul>"
         x += "</li>"
         return x
 
     
-    for j in catagories:
-        htmltag += "<li>"+ str(j) 
-        print(j)
-        htmltag = rec(j.id, htmltag)
+    for cat in catagories:
+        htmltag += "<li>"+ cat.name
+        #print(cat)
+        htmltag = recur(cat.id, htmltag)
 
     htmltag += "</ul>"
 
